64895ffff7d6e0197ed8e831e1cd7b8204d3f896
tdownes57 <tomdownes1@gmail.com>
2023-05-31 00:58:10 -0700

    Rubiks_Python project

---
+++ Rubiks_Python/Rubiks_Python.py
@@ -0,0 +1,33 @@
+
+# AI based Rubik's Cube Solver using Flutter and Python
+# https://github.com/brinesoftwares/Flutter-AI-Rubik-cube-Solver
+
+
+
+#  https://math.berkeley.edu/~hutching/rubik.pdf
+
+
+#The mathematics of Rubik’s Cube
+
+#Michael Hutchings
+
+#Department of Mathematics
+
+#University of California, Berkeley
+
+#BAMC Colloquium
+
+#UC Berkeley
+
+#December 11, 2022
+
+#Michael
+
+
+
+
+
+
+
+
+
