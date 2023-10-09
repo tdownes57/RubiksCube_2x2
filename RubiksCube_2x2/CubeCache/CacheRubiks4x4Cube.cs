d6129569180afd6902f7c1ff252b604d28568fbf
tdownes57 <tomdownes1@gmail.com>
2023-10-09 09:58:45 -0700

    public void ClearCache_RebuildFromFactory() private void ClearCache_RebuildTilesOnly()

--- RubiksCube_2x2/CubeCache/CacheRubiks4x4Cube.cs
+++ RubiksCube_2x2/CubeCache/CacheRubiks4x4Cube.cs
@@ -4,8 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RubiksCube_2x2.CubeCache
+namespace RubiksCube_2x2
 {
+    public enum EnumColors
+    {
+        //   ---10/09/2023 td
+        Undetermined, // = 0,
+        Red, //  = 1,
+        Yellow, //  = 2,
+        Orange, //  = 3,
+        Blue, //  = 4,
+        Green, //  = 5,
+        White, //  = 6
+    }
+
     internal class CacheRubiks4x4Cube
     {
         //
@@ -51,9 +63,10 @@ namespace RubiksCube_2x2.CubeCache
         public CacheRubiks4x4Cube(bool param_buildNew)
         {
             // Added 10/09/2023 td
-            if (param_buildNew) ClearCache_RebuildFromFactory()
+            if (param_buildNew) ClearCache_RebuildFromFactory();
 
         }
+
 
         public void ClearCache_RebuildFromFactory()
         {
@@ -64,13 +77,66 @@ namespace RubiksCube_2x2.CubeCache
             //
             // All sides will be uni-color... entroy level: zero(0).  
             //
+            //----My4x4Cube = new RubiksCubeBothSides();
 
+            // Encapsulated 10/09/2023  
+            ClearCache_RebuildTilesOnly(); 
 
+            //
+            // Find a way to connect the construct the 
+            //   cube (from factory specifications!!) 
+            //   using the new tiles!!
+            //
+            // Probably, we need to call a constructor for the
+            // new cube which will accept our face tiles as 
+            // the six-times-four(24) parameters.  
+            //
+            //    ----10/09/2023 thomas downes
+            //
 
 
         }
 
 
+            private void ClearCache_RebuildTilesOnly()
+            {
+                //
+                // Added 10/09/2023  
+                //
+                MyTileRed1 = new RubiksFaceTile_Class(EnumColors.Red); // MyTileRed1;
+            MyTileRed2 = new RubiksFaceTile_Class(EnumColors.Red); //  MyTileRed2;
+            MyTileRed3 = new RubiksFaceTile_Class(EnumColors.Red); //  MyTileRed3;
+            MyTileRed4 = new RubiksFaceTile_Class(EnumColors.Red); //  MyTileRed4;
+
+            MyTileYellow1 = new RubiksFaceTile_Class(EnumColors.Yellow); //  MyTileYellow1;
+            MyTileYellow2 = new RubiksFaceTile_Class(EnumColors.Yellow); //  MyTileYellow2;
+            MyTileYellow3 = new RubiksFaceTile_Class(EnumColors.Yellow); //  MyTileYellow3;
+            MyTileYellow4 = new RubiksFaceTile_Class(EnumColors.Yellow); //  MyTileYellow4;
+
+        public RubiksFaceTile_Class(EnumColors.Orange); //   MyTileOrange1;
+        public RubiksFaceTile_Class(EnumColors.Orange); //   MyTileOrange2;
+        public RubiksFaceTile_Class(EnumColors.Orange); //   MyTileOrange3;
+        public RubiksFaceTile_Class(EnumColors.Orange); //   MyTileOrange4;
+
+        public RubiksFaceTile_Class MyTileBlue1;
+        public RubiksFaceTile_Class MyTileBlue2;
+        public RubiksFaceTile_Class MyTileBlue3;
+        public RubiksFaceTile_Class MyTileBlue4;
+
+        public RubiksFaceTile_Class MyTileGreen1;
+        public RubiksFaceTile_Class MyTileGreen2;
+        public RubiksFaceTile_Class MyTileGreen3;
+        public RubiksFaceTile_Class MyTileGreen4;
+
+        public RubiksFaceTile_Class MyTileWhite1;
+        public RubiksFaceTile_Class MyTileWhite2;
+        public RubiksFaceTile_Class MyTileWhite3;
+        public RubiksFaceTile_Class MyTileWhite4;
+
 
     }
+
+
+
+}
 }
